3a14a0673531303a51e928e7a041b271dda183f1
Yan <kudashovian2006@gmail.com>
2025-11-03 12:47:55 +0500

    update server-client interface

--- src/main/java/com/bya/client/HeroClient.java
+++ src/main/java/com/bya/client/HeroClient.java
@@ -1,28 +1,25 @@
 package com.bya.client;
 
-import com.bya.model.ClientRequest;
-import com.bya.model.HeroAttackData;
-import com.bya.model.ServerEvent;
+import com.bya.model.*;
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.Map;
 import java.util.Scanner;
 
 public class HeroClient {
 
-    private static final String SERVER_IP = "172.25.144.1"; // <-- ИЗМЕНИТЬ НА IP СЕРВЕРA
+    private static final String SERVER_IP = "172.25.144.1";
     private static final int SERVER_PORT = 12345;
     private static int myHp = 100;
     private static volatile boolean isGameOver = false;
+    private static final Gson gson = new Gson();
 
     public static void main(String[] args) {
-        Gson gson = new Gson();
-
         try (Socket socket = new Socket(SERVER_IP, SERVER_PORT)) {
             PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
             BufferedReader serverReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
@@ -31,8 +28,7 @@ public class HeroClient {
                 try {
                     String serverJson;
                     while ((serverJson = serverReader.readLine()) != null) {
-                        ServerEvent event = gson.fromJson(serverJson, ServerEvent.class);
-                        handleServerEvent(event);
+                        handleServerEvent(serverJson);
                     }
                 } catch (IOException e) {
                     if (!isGameOver) {
@@ -66,30 +62,33 @@ public class HeroClient {
         }
     }
 
-    private static void handleServerEvent(ServerEvent event) {
+    private static void handleServerEvent(String serverJson) {
         if (isGameOver) return;
 
-        Map<String, Object> data = event.data;
-        switch (event.eventType) {
+        JsonObject eventObject = gson.fromJson(serverJson, JsonObject.class);
+        String eventType = eventObject.get("eventType").getAsString();
+        JsonObject dataObject = eventObject.getAsJsonObject("data");
+
+        switch (eventType) {
             case "attackConfirmation":
-                String message = (String) data.get("message");
-                double villainHp = (Double) data.get("villainHp");
-                System.out.println("Ответ сервера: " + message + " | Здоровье злодея: " + (int) villainHp);
+                AttackConfirmationData confirmData = gson.fromJson(dataObject, AttackConfirmationData.class);
+                System.out.println("Ответ сервера: " + confirmData.message + " | Здоровье злодея: " + confirmData.villainHp);
                 break;
+
             case "villainAttack":
-                double damage = (Double) data.get("damageDealt");
-                myHp -= (int) damage;
-                System.out.println("Злодей атакует! Вы получили " + (int) damage + " урона. Ваше здоровье: " + myHp);
+                VillainAttackData attackData = gson.fromJson(dataObject, VillainAttackData.class);
+                myHp -= attackData.damageDealt;
+                System.out.println("Злодей атакует! Вы получили " + attackData.damageDealt + " урона. Ваше здоровье: " + myHp);
                 if (myHp <= 0) {
                     System.out.println("Вы были повержены...");
-                    // Здесь можно было бы отправить серверу сообщение о своем поражении
                 }
                 break;
+
             case "gameOver":
                 isGameOver = true;
-                String winner = (String) data.get("winner");
+                GameOverData gameOverData = gson.fromJson(dataObject, GameOverData.class);
                 System.out.println("==========================================");
-                System.out.println("ИГРА ОКОНЧЕНА! Победитель: " + winner);
+                System.out.println("ИГРА ОКОНЧЕНА! Победитель: " + gameOverData.winner);
                 System.out.println("==========================================");
                 break;
         }
